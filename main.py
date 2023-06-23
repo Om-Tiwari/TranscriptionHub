870df3cb1b5ddbfb0ad1c38579d5ed95e9f8c6ed
Om Tiwari <omthegreat085@gmail.com>
2023-06-23 16:27:28 +0530

    formatting

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import assemblyai as aai
 import os
 from dotenv import load_dotenv
 from gevent.pywsgi import WSGIServer
-load_dotenv()
 
+load_dotenv()
 app = Flask(__name__)
 
+def transcribe():
+    aai.settings.api_key = os.getenv("AAI_API_KEY")
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe("media/audio.wav")
+    return transcript.text
 
 @app.route('/transcript')
 def transcript():
@@ -21,13 +26,6 @@ def home():
         audio.save("media/audio.wav")
         return redirect(url_for('transcript'))
     return render_template('index.html', title='Home')
-
-
-def transcribe():
-    aai.settings.api_key = os.getenv("AAI_API_KEY")
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe("media/audio.wav")
-    return transcript.text
 
 
 http_server = WSGIServer(('', 5000), app)
